b27b99454e9c72f67ca13fdeaf0d29598d84e647
jily <jily1218@163.com>
2025-01-16 18:02:57 +0800

    前台-收藏：已经有userId的情况下，Parameter 'userId' not found.的问题

--- jy-study-lesson/src/main/java/com/jy/study/lesson/mapper/StudyUserCollectMapper.java
+++ jy-study-lesson/src/main/java/com/jy/study/lesson/mapper/StudyUserCollectMapper.java
@@ -1,6 +1,7 @@
 package com.jy.study.lesson.mapper;
 
 import com.jy.study.lesson.domain.StudyUserCollect;
+import org.apache.ibatis.annotations.Param;
 
 public interface StudyUserCollectMapper {
     public int insertUserCollect(StudyUserCollect collect);
@@ -9,5 +10,5 @@ public interface StudyUserCollectMapper {
     
     public int deleteUserCollect(Long userId, String type, Long targetId);
     
-    public boolean checkCollected(Long userId, String type, Long targetId);
-} 
+    public boolean checkCollected(@Param("userId") Long userId, @Param("type") String type, @Param("targetId") Long targetId);
+}
