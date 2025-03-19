d7146f03f8c028da177218e7a8b62b1fe31b1af5
jily <jily1218@163.com>
2025-03-19 22:19:01 +0800

    feat(web): 添加个人中心功能 - 新增个人中心页面，包括基本资料和安全设置两个标签页 - 实现用户信息修改和密码修改功能

---
+++ jy-study-admin/src/main/java/com/jy/study/web/controller/WebProfileController.java
@@ -0,0 +1,80 @@
+package com.jy.study.web.controller;
+
+import com.jy.study.common.utils.DateUtils;
+import com.jy.study.common.utils.ShiroUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import com.jy.study.common.core.controller.BaseController;
+import com.jy.study.common.core.domain.AjaxResult;
+import com.jy.study.common.core.domain.entity.SysUser;
+import com.jy.study.system.service.ISysUserService;
+import com.jy.study.framework.shiro.service.SysPasswordService;
+import com.jy.study.common.utils.ShiroUtils;
+import com.jy.study.common.utils.DateUtils;
+
+@Controller
+@RequestMapping("/web/user/profile")
+public class WebProfileController extends BaseController {
+    
+    @Autowired
+    private ISysUserService userService;
+    
+    @Autowired
+    private SysPasswordService passwordService;
+
+    /**
+     * 个人中心页面
+     */
+    @GetMapping()
+    public String profile(ModelMap mmap) {
+        SysUser user = getSysUser();
+        mmap.put("user", user);
+        return "web/user/profile";
+    }
+
+    /**
+     * 修改用户信息
+     */
+    @PostMapping("/update")
+    @ResponseBody
+    public AjaxResult updateProfile(SysUser user) {
+        SysUser currentUser = getSysUser();
+        currentUser.setUserName(user.getUserName());
+        currentUser.setEmail(user.getEmail());
+        currentUser.setPhonenumber(user.getPhonenumber());
+        currentUser.setSex(user.getSex());
+        if (userService.updateUserInfo(currentUser) > 0) {
+            setSysUser(userService.selectUserById(currentUser.getUserId()));
+            return success();
+        }
+        return error();
+    }
+
+    /**
+     * 修改密码
+     */
+    @PostMapping("/updatePwd")
+    @ResponseBody
+    public AjaxResult updatePwd(String oldPassword, String newPassword) {
+        SysUser user = getSysUser();
+        if (!passwordService.matches(user, oldPassword)) {
+            return error("修改密码失败，原密码错误");
+        }
+        if (passwordService.matches(user, newPassword)) {
+            return error("新密码不能与原密码相同");
+        }
+        
+        // 更新用户密码信息
+        user.setSalt(ShiroUtils.randomSalt());  // 生成新的随机盐值
+        user.setPassword(passwordService.encryptPassword(user.getLoginName(), newPassword, user.getSalt()));  // 加密新密码
+        user.setPwdUpdateDate(DateUtils.getNowDate());
+
+        if (userService.resetUserPwd(user) > 0) {
+            setSysUser(userService.selectUserById(user.getUserId()));
+            return success();
+        }
+        return error("修改密码异常，请联系管理员");
+    }
+} 
--- jy-study-framework/src/main/java/com/jy/study/framework/config/ShiroConfig.java
+++ jy-study-framework/src/main/java/com/jy/study/framework/config/ShiroConfig.java
@@ -324,17 +324,17 @@ public class ShiroConfig
         filterChainDefinitionMap.put("/web/css/**", "anon");
         filterChainDefinitionMap.put("/web/js/**", "anon");
         filterChainDefinitionMap.put("/web/images/**", "anon");
-        //把这个大的放在之后，才不会覆盖掉login导致不使用验证码
+        // 需要登录才能访问的接口，要放在/web/**之前
+        filterChainDefinitionMap.put("/web/user/**", "authc");
+
+        // 其他web路径允许匿名访问
         filterChainDefinitionMap.put("/web/**","anon");
-        filterChainDefinitionMap.put("/web","anon");//而且还要把也加上user不然不知道为什么加载不出
+        filterChainDefinitionMap.put("/web","anon");
 
         // 首页和公共页面允许匿名访问
         filterChainDefinitionMap.put("/web/index", "anon");
         filterChainDefinitionMap.put("/web/public/**", "anon");
         
-        // 需要登录才能访问的接口
-        filterChainDefinitionMap.put("/web/user/**", "user");
-
         Map<String, Filter> filters = new LinkedHashMap<String, Filter>();
         filters.put("onlineSession", onlineSessionFilter());
         filters.put("syncOnlineSession", syncOnlineSessionFilter());
